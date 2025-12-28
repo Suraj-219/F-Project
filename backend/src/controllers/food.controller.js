ac21d19606b81339de4c903c8e2c489df4c04d5c
Suraj Pal <surajpal7442@gmail.com>
2025-12-28 16:57:21 +0530

    add user authentication middleware

--- backend/src/controllers/food.controller.js
+++ backend/src/controllers/food.controller.js
@@ -33,6 +33,17 @@ async function createFood(req, res) {
     })
 }
 
+
+async function getFoodItems(req, res) {
+    
+    const foodItems = await foodModel.find({})
+    res.status(200).json({
+        message: "Food items fetched successfully",
+        foodItems
+    })
+}
+
 module.exports = {
-    createFood
+    createFood,
+    getFoodItems
 }
